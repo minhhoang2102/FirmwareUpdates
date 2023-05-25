ef4b640a33e8e3e5bdb5ab39d72b9ba9f007fd68
minhhoang2102 <117298762+minhhoang2102@users.noreply.github.com>
2023-05-25 23:12:54 +0700

    Update SIM7600E.py

--- SIM7600E.py
+++ SIM7600E.py
@@ -1,17 +1,18 @@
 from machine import UART, Pin
-import machine
+from math import radians, cos, sin, asin, sqrt
+import math
 import utime
 
 #indicate program started visually
-led_onboard = machine.Pin(25, machine.Pin.OUT)
-led_onboard.value(0)     # onboard LED OFF/ON for 0.5/1.0 sec
-utime.sleep(0.5)
-led_onboard.value(1)
-utime.sleep(1.0)
-led_onboard.value(0)
+# led_onboard = machine.Pin(25, machine.Pin.OUT)
+# led_onboard.value(0)     # onboard LED OFF/ON for 0.5/1.0 sec
+# utime.sleep(0.5)
+# led_onboard.value(1)
+# utime.sleep(1.0)
+# led_onboard.value(0)
 
 uart0 = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
-uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))  # for ATmega328P
+# uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5))  # for ATmega328P
 # print(uart0)
 
 def sendCMD_waitResp(cmd, timeout):
@@ -59,13 +60,15 @@ def ListDirFile(wtype):
 def TransFileCMD(filepath, location, size):
     return sendCMD_waitResp('AT+CFTRANTX=' + '\"' + filepath + '\",' + location + ',' + size, 1000)
 def TransFileCMD_Full(filepath):
-    return sendCMD_waitResp('AT+CFTRANTX=' + '\"' + filepath + '\"', 3000)
+    return sendCMD_waitResp('AT+CFTRANTX=' + '\"' + filepath + '\"', 5000)
 def FileInfo(filename):
     return sendCMD_waitResp('AT+FSATTRI=' + filename, 1000)
 def FileSize(filename):
     rx_str = FileInfo(filename)
     list_str = rx_str.splitlines()
     return list_str[2][9:14]
+def DeleteFile(filename):
+    sendCMD_waitResp('AT+FSDEL=' + filename, 1000)
 #----------------------------------------------------#
 
 #--------------Commands for FTPS--------------#
@@ -74,11 +77,11 @@ def StartFTP():
 def StopFTP():
     sendCMD_waitResp('AT+CFTPSSTOP', 1000)
 def LoginFTPServer(host, port, username, password, server_type):
-    sendCMD_waitResp('AT+CFTPSLOGIN='+'\"'+host+'\",'+port+',\"'+username+'\",'+'\"'+password+'\",'+server_type, 1000)
+    sendCMD_waitResp('AT+CFTPSLOGIN='+'\"'+host+'\",'+port+',\"'+username+'\",'+'\"'+password+'\",'+server_type, 3000)
 def LogoutFTPServer():
     sendCMD_waitResp('AT+CFTPSLOGOUT', 1000)
 def ChangeDir(dir_name):
-    sendCMD_waitResp('AT+CFTPSCWD='+'\"'+dir_name+'\"', 1000)
+    sendCMD_waitResp('AT+CFTPSCWD='+'\"'+dir_name+'\"', 2000)
 def GetCurrentDir():
     sendCMD_waitResp('AT+CFTPSPWD', 1000)
 def ListItems(dir_name):
@@ -86,10 +89,10 @@ def ListItems(dir_name):
 def downloadFromFTP(filename):
     #dir_in: 1-F:/ 2-D:/ 3-E:/
     print('Downloading file from FTP...')
-    sendCMD_waitResp('AT+CFTPSGETFILE='+'\"'+filename+'\"', 1000)
+    sendCMD_waitResp('AT+CFTPSGETFILE='+'\"'+filename+'\"', 3000)
 def uploadToFTP(filename, dir_in):
 	print('Uploading file from FTP...')
-	send_at('AT+CFTPSPUTFILE='+'\"'+filename+'\"', 1000)
+	send_at('AT+CFTPSPUTFILE='+'\"'+filename+'\"', 3000)
 #--------------------------------------------#
 
 #--------------Commands for GPS--------------#
@@ -98,9 +101,42 @@ def StartGPS():
 def StopGPS():
     sendCMD_waitResp('AT+CGPS=0', 1000)
 def GetGPSinfo():
-    sendCMD_waitResp('AT+CGPSINFO', 1000)
+    return sendCMD_waitResp('AT+CGPSINFO', 1000)
 def StrtGPSauto(auto):
     sendCMD_waitResp('AT+CGPS=' + auto, 1000)
+    #Return lists of latitude and longitude    
+def ReadPosition(RxStr):
+    RxStr = RxStr.split("+CGPSINFO: ")
+    
+    #calculate position
+    lat_raw = ''
+    lon_raw = ''
+    lat_raw = RxStr[1][0:11]
+    lon_raw = RxStr[1][14:26]
+    lat_raw1 = float(lat_raw[0:2])
+    lat_raw2 = float(lat_raw[2:12])
+    lon_raw1 = float(lon_raw[0:3])
+    lon_raw2 = float(lon_raw[3:13])
+    lat = lat_raw1 + lat_raw2/60
+    lon = lon_raw1 + lon_raw2/60
+#     print('lat: ', lat_raw)
+#     print('lon: ', lon_raw)
+    a = (lat, lon)
+    return a
+
+def ReadDistance(lat1, lon1, lat2, lon2):
+    #convert degrees to radians
+    lon1 = radians(lon1)
+    lat1 = radians(lat1)
+    lon2 = radians(lon2)
+    lat2 = radians(lat2)
+    
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    distance = 2 * asin(sqrt(a)) * 6378100 #6378100 is the radius of the Earth
+    return distance
 #--------------------------------------------#
     
 #____________________Transfer HEX file to Raspberry Pi Pico____________________#
@@ -153,42 +189,38 @@ def GetRxHexStr(filepath):
     return RawFile
 #______________________________________________________________________________#
 
-def downloadFirmware(filename):
-    #FTP Server
-    ChangeDir('Firmware_Updates')
-    downloadFromFTP(filename)
+# response = GetGPSinfo()
+# res_list = response.split('\r\n')
+# if len(res_list[1]) == 19:
+#     print('waiting for GPS info...')
+# print('gps info:', res_list[1])
+# print('size of res:', len(res_list[1]))
+
     
-    #File Transfer
-    return GetRxHexStr('F:/' + filename)
- 
+# while True:
+#     x = GetGPSinfo()
+#     y = ReadPosition(x)
+#     lat = y[0]
+#     lon = y[1]
+# #     print('latitude: ',ReadPosition(x))
+#     dist = ReadDistance(DHBKgps[0], DHBKgps[1], lat, lon)
+#     print('dist =', dist/1000)
+#     utime.sleep(5)
 
 # filename = 'Blink2sec.ino.hex'
-# TransFileCMD_Full('F:/'+filename)
 # filename = 'Blink2sec.ino.hex'
-# StopFTP()
 
-# utime.sleep(0.1)
-# LoginFTPServer('ftp.drivehq.com', '21', 'mh1911200', '98027435610', '0')
-# utime.sleep(0.1)
-# ChangeDir('Firmware_Updates')
-# utime.sleep(0.1)
-# downloadFromFTP(filename)
-# utime.sleep(0.1)
-# LogoutFTPServer()
-# utime.sleep(0.1)
-# StopFTP()
-# utime.sleep(0.1)
 #----------------------------main----------------------------#
-
+# StartFTP()
 # LoginFTPServer('ftp.drivehq.com', '21', 'mh1911200', '98027435610', '0')
-
-# f = GetRxHexStr('F:/Blinkfast.ino.hex')
-# print(f)
-# print('size:', len(f))
-# print('hexstr[-4:-2]:', RxString[-4:-2])
-# GetRxHexStr(hexstr)
-
-# sendCMD_waitResp('AT+IPREX=9600', 2000)
-# sendCMD_waitResp('AT+CFTRANTX=' + '\"' + 'F:/Blinkfast.ino.hex' + '\"', 3000)
+# utime.sleep(3)
+# ChangeDir('Firmware_Updates')
+# utime.sleep(3)
+# downloadFromFTP('version.txt')
+# utime.sleep(2)
+# LogoutFTPServer()
+# utime.sleep(2)
+# StopFTP()
+# ListDirFile('0')
 
 
